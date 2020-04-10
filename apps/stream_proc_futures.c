214e0a8fa18f4743b27d219e7cd35492c95393ae
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-04-10 02:19:59 -0400

    Fixed a spelling error when calling getpid()

--- apps/stream_proc_futures.c
+++ apps/stream_proc_futures.c
@@ -191,7 +191,7 @@ void stream_consumer_future(int32 id, future_t* fut)
     }
     tscdf_free(tptr);
     future_free(fut);
-    kprintf("process %d exited\n", getpit());
+    kprintf("process %d exited\n", getpid());
     ptsend(pcport, getpid());
     return;
 }
