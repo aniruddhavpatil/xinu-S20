77f562ffc73ac515b41f5de549d4ccae1715641b
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-05-03 21:03:38 -0400

    Fixed repeated decrement of nlinks

--- system/fs.c
+++ system/fs.c
@@ -529,7 +529,7 @@ int fs_unlink(char *filename)
     int get_inode_status = fs_get_inode_by_num(0, fsd.root_dir.entry[file_num].inode_num, &in);
     if (get_inode_status == SYSERR) return SYSERR;
     if (in.nlink > 1){
-        fsd.root_dir.entry[file_num].name[0] = '\0';
+        // fsd.root_dir.entry[file_num].name[0] = '\0';
         fsd.root_dir.numentries--;
         in.nlink--;
         fs_put_inode_by_num(0, fsd.root_dir.entry[file_num].inode_num, &in);
@@ -537,11 +537,11 @@ int fs_unlink(char *filename)
     else if (in.nlink == 1) {
         for(int i = 0; i < in.size; i++){
             fs_clearmaskbit(in.blocks[i]);
-            fsd.root_dir.entry[file_num].name[0] = '\0';
-            fsd.root_dir.numentries--;
-            in.nlink--;
-            fs_put_inode_by_num(0, fsd.root_dir.entry[file_num].inode_num, &in);
         }
+        // fsd.root_dir.entry[file_num].name[0] = '\0';
+        fsd.root_dir.numentries--;
+        in.nlink--;
+        fs_put_inode_by_num(0, fsd.root_dir.entry[file_num].inode_num, &in);
     }
     else return SYSERR;
 
